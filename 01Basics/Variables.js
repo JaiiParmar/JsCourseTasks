4c4e3e52b21e0dc6d1d60a544b6d320a56e3d27a
Jai Parmar <jaiparmar207@gmail.com>
2020-02-14 22:16:46 +0530

    Init commit + SomeBasic

---
+++ 01Basics/Variables.js
@@ -0,0 +1,15 @@
+let name = `John`;
+console.log(name)
+
+let bonus = 20
+let score = 100
+
+let total = bonus + score
+
+console.log(total)
+
+let first_name = 'Jai'
+let last_name = 'Parmar'
+let full_name = first_name + " " + last_name
+console.log(full_name)
+
---
+++ 01Basics/nullandbool.js
@@ -0,0 +1,6 @@
+// temp is coming from a website API
+
+var temp = 0
+
+console.log("Current temprature is " + temp)
+
