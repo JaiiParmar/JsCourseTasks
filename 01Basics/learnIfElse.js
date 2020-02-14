7ea1660daed360699282bc0c441d5701cc38faa4
Jai Parmar <jaiparmar207@gmail.com>
2020-02-14 22:37:44 +0530

    lesson 5

---
+++ 01Basics/learnIfElse.js
@@ -0,0 +1,41 @@
+// if (false) {
+//     console.log("I am inside if Block")
+//     console.log('I am stil inside if block')
+// }
+
+// else if (true ) {
+//     console.log("Notice this part.")
+// }
+
+// else {
+
+//     console.log("I am inside if")
+// }
+
+
+// LCO
+
+var whoIsHer = 'user'
+
+if (whoIsHer === 'user') {
+    console.log('Greeting message, user')
+    console.log("Allow access to view all courses.")
+}
+else if (whoIsHer === 'user') {
+    console.log('Greeting message, teacher.')
+    console.log("Allow access to view all courses.")
+}
+else {
+    console.log('MESSAGE : varify your email.')
+    console.log('Send user an email.')
+}
+
+let grades = 10 
+if (grades >= 10)
+    console.log(`Amazing`)
+else if (grades >= 5)
+    console.log(`Good`)
+else if (grades >= 3)
+    console.log(`poor`)
+else
+    console.log('fail')
--- 01Basics/nullandbool.js
+++ 01Basics/nullandbool.js
@@ -1,6 +1,22 @@
 // temp is coming from a website API
 
-var temp = 0
+var temp = 0;
 
-console.log("Current temprature is " + temp)
+// done some calculation
+// temp = 2
 
+console.log("Current temprature is " + temp);
+
+//Grade System
+
+// 10 - Great job
+// 5 - work harder
+// 2 - poor
+// 0 - fail
+
+// Boolean - true/false
+
+let Grade = true;
+let actualMarks = 7;
+let myGrade = actualMarks = 10;
+console.log(myGrade);
